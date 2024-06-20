1d172956a684e2028c57ddf6468b3ca305c18ce4
JuanUbillus <juan.ubillusr@gmail.com>
2024-06-19 22:25:31 -0500

    revisar

--- app/Http/Controllers/NiubizPaymentController.php
+++ app/Http/Controllers/NiubizPaymentController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Helpers\VisaHelper;
-use App\Models\producto;
+use App\Models\Producto;
 use Illuminate\Http\Request;
 
 class NiubizPaymentController extends Controller
@@ -14,6 +14,50 @@ class NiubizPaymentController extends Controller
     }
 
     public function comprar (Request $request){
+
+        $initialProducts = [
+            [
+                'nombre' => 'producto1',
+                'precio' => 50.00
+            ],
+            [
+                'nombre' => 'producto2',
+                'precio' => 150.00
+            ],
+            [
+                'nombre' => 'producto3',
+                'precio' => 250.00
+            ],
+        ];
+
+        // Obtener los productos de la sesión, si no existe inicializar con los ítems iniciales
+        $cart = session()->get('cart', $initialProducts);
+
+        // Guardar los productos en la sesión si aún no están guardados
+        if (!session()->has('cart')) {
+            session(['cart' => $cart]);
+        }
+
+
+
+        // Obtener los productos de la sesión
+        $cart = session()->get('cart', []);
+
+        // Calcular la suma de los precios de los productos en el carrito
+        $total = 0;
+        foreach ($cart as $product) {
+            $total += $product['precio'];
+        }
+        dd($total);
+
+
+
+
+
+
+
+
+
         $producto = Producto::find($request->id);
         $token =  VisaHelper::generateToken();
         $session = VisaHelper::generateSession($producto->price,$token);
@@ -30,7 +74,8 @@ class NiubizPaymentController extends Controller
 
         $token = VisaHelper::generateToken();
         $data = VisaHelper::generateAuthorization($amount, $purchaseNumber, $transactionToken, $token);
-
+        //TODO registrar la compra en tu bd
+        dd($data);
         return view('productos.finalizar', compact('data','purchaseNumber'));
     }
 }
