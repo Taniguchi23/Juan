71e2a58927261dfbfbe8ffc5cd4b4f4c7c896a60
JuanUbillus <juan.ubillusr@gmail.com>
2024-07-15 06:29:54 -0500

    comentarios

--- app/Helpers/VisaHelper.php
+++ app/Helpers/VisaHelper.php
@@ -4,9 +4,9 @@ namespace App\Helpers;
 
 class VisaHelper
 {
-    const VISA_DEVELOPMENT = true;
+    const VISA_DEVELOPMENT = true; //constante para el modo de desarrollo
 
-    // Desarrollo Visa
+    // Desarrollo Visa  || constantes para las credenciales y URLs de desarrollo
     const VISA_DEV_MERCHANT_ID = '456879852';
     const VISA_DEV_USER = 'integraciones@niubiz.com.pe';
     const VISA_DEV_PWD = '_7z3@8fF';
@@ -15,7 +15,7 @@ class VisaHelper
     const VISA_DEV_URL_JS = 'https://static-content-qas.vnforapps.com/v2/js/checkout.js?qa=true';
     const VISA_DEV_URL_AUTHORIZATION = 'https://apisandbox.vnforappstest.com/api.authorization/v3/authorization/ecommerce/'.self::VISA_DEV_MERCHANT_ID;
 
-    // Producción Visa
+    // Producción Visa  || constantes para las credenciales y URLs de produccion
     const VISA_PRD_MERCHANT_ID = '527127703';
     const VISA_PRD_USER = 'integraciones.visanet@necomplus.com';
     const VISA_PRD_PWD = 'd5e7nk$M';
@@ -24,7 +24,7 @@ class VisaHelper
     const VISA_PRD_URL_JS = 'https://static-content.vnforapps.com/v2/js/checkout.js';
     const VISA_PRD_URL_AUTHORIZATION = 'https://apiprod.vnforapps.com/api.authorization/v3/authorization/ecommerce/'.self::VISA_PRD_MERCHANT_ID;
 
-    // Configuración visa
+    // Configuración visa || configuracion dinamica basada en el nodo 
     const VISA_MERCHANT_ID = self::VISA_DEVELOPMENT ? self::VISA_DEV_MERCHANT_ID : self::VISA_PRD_MERCHANT_ID;
     const VISA_USER = self::VISA_DEVELOPMENT ? self::VISA_DEV_USER : self::VISA_PRD_USER;
     const VISA_PWD = self::VISA_DEVELOPMENT ? self::VISA_DEV_PWD : self::VISA_PRD_PWD;
@@ -35,27 +35,27 @@ class VisaHelper
 
     public static function generateToken()
     {
-        $curl = curl_init();
+        $curl = curl_init(); //inicia una nueva session
         curl_setopt_array($curl, array(
             CURLOPT_URL => self::VISA_URL_SECURITY,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_HTTPHEADER => array(
+            CURLOPT_RETURNTRANSFER => true, //devuelve una cadena de texto llamada curl_Exec
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1, //version del protocolo HTTP
+            CURLOPT_CUSTOMREQUEST => "POST", //se usa para la solicitud
+            CURLOPT_HTTPHEADER => array( //incluye la autorizacion en base64
                 "Accept: */*",
                 'Authorization: '.'Basic '.base64_encode(self::VISA_USER.":".self::VISA_PWD)
             ),
         ));
-        $response = curl_exec($curl);
-        curl_close($curl);
-        return $response;
+        $response = curl_exec($curl); //ejecuta la solicitud
+        curl_close($curl); //cierra la sesion cURL
+        return $response; //devuelve la respuesta del API de seguridad de VISA
     }
 
     public static function generateSession($amount, $token)
-    {
+    { //se define los datos necesarios para la sesion de pago
         $session = array(
-            'amount' => $amount,
-            'antifraud' => array(
+            'amount' => $amount, //monto de la transaccion
+            'antifraud' => array( //define la informacion antifraude como la IP del cliente y datos especificos
                 'clientIp' => $_SERVER['REMOTE_ADDR'],
                 'merchantDefineData' => array(
                     'MDD15' => "Valor MDD 15",
@@ -63,54 +63,55 @@ class VisaHelper
                     'MDD33' => 'Valor MDD 30'
                 ),
             ),
-            'channel' => 'web',
+            'channel' => 'web', //canal de la transaccion
         );
-        $json = json_encode($session);
-        $response = json_decode(self::postRequest(self::VISA_URL_SESSION,$json, $token));
+        $json = json_encode($session); //convierte el array de la sesion en un JSON
+        $response = json_decode(self::postRequest(self::VISA_URL_SESSION,$json, $token)); //envia la solicitud POST con los datos de sesion y el token de seguridad  
+        //convirte la respuesta JSON en un objeto PHP
         return $response->sessionKey;
     }
 
     public static function generateAuthorization($amount, $purchaseNumber, $transactionToken, $token)
     {
-        $data = array(
-            'antifraud' => null,
-            'captureType' => 'manual',
-            'channel' => 'web',
-            'countable' => true,
-            'order' => array(
+        $data = array( //se genera los datos necesacios para la autoriazacion de pagos
+            'antifraud' => null, //informacion antifraude, en este caso null
+            'captureType' => 'manual', //tipo de captura
+            'channel' => 'web', //canal de transaccion
+            'countable' => true, //indica si la transaccion es contable
+            'order' => array( //informacion de la orden como el monto,moneda,numero de compra y token de transaccion
                 'amount' => $amount,
                 'currency' => 'PEN',
                 'purchaseNumber' => $purchaseNumber,
                 'tokenId' => $transactionToken
             ),
-            'recurrence' => null,
+            'recurrence' => null,//informacion de recurrencia
             'sponsored' => null
         );
-        $json = json_encode($data);
-        $session = json_decode(self::postRequest(self::VISA_URL_AUTHORIZATION, $json, $token));
-        return $session;
+        $json = json_encode($data);//convierte el array obtenido en un JSON
+        $session = json_decode(self::postRequest(self::VISA_URL_AUTHORIZATION, $json, $token));//envia la solicitud POST con los datos y el token de seguridad
+        return $session; //devuelve la respuesta de autorizacion de pago
     }
 
     private static function postRequest($url, $postData, $token)
     {
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => $url,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_HTTPHEADER => array(
+        $curl = curl_init(); //inicia una nueva sesion cURL
+        curl_setopt_array($curl, array( //se establece un array de opciones para la transferencia
+            CURLOPT_URL => $url, //url del endpoint
+            CURLOPT_RETURNTRANSFER => true, //si es true devuelve en una cadena de texto llamada curl_exec
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1, //usa la version 1.1 del protocolo HTTP
+            CURLOPT_CUSTOMREQUEST => "POST", //el metodo que se usa para la solicitud es POST
+            CURLOPT_HTTPHEADER => array(//cabecera http que se incluiran en la solicitud 
                 'Authorization: '.$token,
                 'Content-Type: application/json'
             ),
-            CURLOPT_POSTFIELDS => $postData
+            CURLOPT_POSTFIELDS => $postData //son los datos a enviar en la solicitud POST
         ));
-        $response = curl_exec($curl);
-        curl_close($curl);
+        $response = curl_exec($curl); //ejecuta la solicitud cURL
+        curl_close($curl); //cierra la sesion cURL
         return $response;
     }
 
-    public static function generatePurchaseNumber()
+    public static function generatePurchaseNumber() //genera un numero de compra unico usando el tiempo y un numero aleatorio
     {
         $purchaseNumber = time() . mt_rand(10, 99);
         return $purchaseNumber;
--- app/Http/Controllers/NiubizPaymentController.php
+++ app/Http/Controllers/NiubizPaymentController.php
@@ -10,25 +10,25 @@ use Illuminate\Http\Request;
 class NiubizPaymentController extends Controller
 {
     public function productos (){
-        $productos = Producto::all();
+        $productos = Producto::all(); //obtiene todos los productos de la bd y lo manda a la vista para mostrarlos
         return view('productos.index', compact('productos'));
     }
 
     public function comprar (Request $request){
-        $producto = Producto::find($request->id);
-        $token =  VisaHelper::generateToken();
-        $session = VisaHelper::generateSession($producto->price,$token);
-        $purchaseNumber = VisaHelper::generatePurchaseNumber();
-        return view('productos.comprar', compact('session','purchaseNumber','producto'));
+        $producto = Producto::find($request->id);//obtiene el producto basado en el ID pasado en la solicitud
+        $token =  VisaHelper::generateToken(); //genera un token de seguridad para la sesion de pago
+        $session = VisaHelper::generateSession($producto->price,$token); //crea una nueva sesion de pago usando el precio y token de seguridad
+        $purchaseNumber = VisaHelper::generatePurchaseNumber(); //crea un numero de compra unico
+        return view('productos.comprar', compact('session','purchaseNumber','producto')); //devuelve la vvista comprar y pasa la sesion numero de compra y producto
     }
 
     public function finalizar(Request $request){
-        $transactionToken = $request->transactionToken;
-        $customerEmail = $request->customerEmail;
-        $amount = $request->amount;
-        $purchaseNumber = $request->purchaseNumber;
-        $channel = $request->channel;
-        $token = VisaHelper::generateToken();
+        $transactionToken = $request->transactionToken; //token de transaccion del cliente
+        $customerEmail = $request->customerEmail; //correo electronico del cliente
+        $amount = $request->amount; //monto del pago
+        $purchaseNumber = $request->purchaseNumber; //numero de compra
+        $channel = $request->channel; //canal de transaccion
+        $token = VisaHelper::generateToken(); // genera un token de seguridad necesario para la AUTORIZACION DE PAGO
         $data = VisaHelper::generateAuthorization($amount, $purchaseNumber, $transactionToken, $token);
 
         // Registrar la compra en la base de datos
